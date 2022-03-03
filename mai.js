0357b961021f7bb91489af25d5cc5d64af6d72c1
jonathanlatalisa <95480314+jonathanlatalisa@users.noreply.github.com>
2022-03-03 10:45:12 -0300

    atualização

---
+++ mai.js
@@ -0,0 +1,20 @@
+function snapCrackle(maxValue) {
+    let resultado = '';
+    for (let i = 0; i <= maxValue; i++) {
+
+        if (i % 2 !== 0 && i % 5 !== 0) {
+            resultado = resultado + 'Snap, ';
+
+        } else if (i % 2 !== 0 && i % 5 == 0) {
+            resultado = resultado + 'SnapCrackle, ';
+
+        } else if (i % 5 == 0) {
+            resultado = resultado + 'Crackle, ';
+
+        } else if (i % 2 === 0 && i % 5 !== 0) {
+            resultado = resultado + i + ', ';
+        }
+    }
+    console.log(resultado);
+}
+snapCrackle(15);
